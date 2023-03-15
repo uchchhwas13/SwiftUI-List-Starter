3718e71161d7710de593c67c10f7e0e0a072eac8
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2023-03-15 12:15:54 +0600

    Initial commit

---
+++ SwiftUI-List-Starter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-List-Starter
+//
+//  Created by Sean Allen on 4/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-List-Starter/SwiftUI_List_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_List_StarterApp.swift
+//  SwiftUI-List-Starter
+//
+//  Created by Uchchhwas.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_List_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
